f4f2de24b0157a73823cfc08d2a3c44149126ff8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-18 16:06:45 +0200

    add Author model and update Book model to use ForeignKey for author

--- books/models.py
+++ books/models.py
@@ -3,20 +3,46 @@
 from django.db import models
 
 
+class Author(models.Model):
+    """Author model."""
+
+    name = models.CharField(max_length=128)
+    birth_date = models.DateField()
+    death_date = models.DateField(null=True)  # maybe still alive
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='authors/',
+                              default='default_author.png')
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+    def __str__(self):
+        """Return the name of the author.
+
+        Returns:
+            str: string representation of the author
+        """
+        return self.name
+
+
 class Book(models.Model):
     """Book model."""
 
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=128)  # change when author is created
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, null=True)
     publication_date = models.DateField()
-    isbn = models.CharField(max_length=13)  # maybe make this a primary key
-    pages = models.IntegerField()
+    pages = models.IntegerField()  # for limiting the current_page feature
     cover = models.ImageField(upload_to='covers/', default='default_book.png')
     buy_link = models.CharField(max_length=255, null=True)
     # avg_rating
     description = models.TextField(blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(default='-')  # for search
 
+    # isbn = models.CharField(max_length=13)  # maybe make this a primary key
     class Meta:
         indexes = [
             models.Index(fields=['title']),
@@ -24,4 +50,9 @@ class Book(models.Model):
         ordering = ["-added_date"]
 
     def __str__(self):
+        """Return the title of the book.
+
+        Returns:
+            str: string representation of the book
+        """
         return self.title
