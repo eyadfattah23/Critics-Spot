4dbf37365d30b5e4cc27e15605e84ea38657fde9
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-21 23:44:37 +0200

    Update signal receiver to use AUTH_USER_MODEL for creating default shelves

--- users/signals.py
+++ users/signals.py
@@ -2,11 +2,11 @@
 """signals for users app."""
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.conf import settings
 from shelves.models import Shelf
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_default_shelves(sender, instance, created, **kwargs):
     """#+
     creates default shelves for a new user when they are created.#+
