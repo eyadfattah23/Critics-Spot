22b96a89cfa76a7f1b0ae86fc33bb69027dd07ae
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-23 22:39:14 +0200

    Enhance admin interfaces: add book count links for Author, Genre, and Shelf models

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """admin Groups handler"""
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from .models import *
 # Register your models here.
 
@@ -10,11 +12,32 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name', 'birth_date', 'death_date',
                     'number_of_books']
 
-    def number_of_books(self, obj):
-        """Count the number of books written by an author."""
-        return obj.book_set.count()
+    def number_of_books(self, author):
+        """Count the number of books written by an author.
+            redirect to books only written by the author"""
+        url = (reverse('admin:books_book_changelist')
+               + '?'
+               + urlencode({'author__id': str(author.id)}))
+        return format_html('<a href="{}">{}</a>', url, author.book_set.count())
+
+        return author.book_set.count()
     number_of_books.short_description = 'Number of books'
 
 
-admin.site.register(Genre)
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'number_of_books']
+
+    def number_of_books(self, genre):
+        """Count the number of books in a genre.
+            redirect to books only in the genre"""
+        url = (reverse('admin:books_book_changelist')
+               + '?'
+               + urlencode({'genre__id': str(genre.id)}))
+        return format_html('<a href="{}">{}</a>', url, genre.books.count())
+
+        return genre.book_set.count()
+    number_of_books.short_description = 'Number of books'
+
+
 admin.site.register(Book)
--- shelves/admin.py
+++ shelves/admin.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """admin Shelves handler"""
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from .models import Shelf, ShelfBook
 # Register your models here.
 
-admin.site.register(Shelf)
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'number_of_books']
+
+    def number_of_books(self, shelf):
+        """Count the number of books in a shelf."""
+        url = (reverse('admin:shelves_shelfbook_changelist')
+               + '?'
+               + urlencode({'shelf__id': str(shelf.id)}))
+        return format_html('<a href="{}">{}</a>', url, shelf.shelfbook_set.count())
+        return shelf.shelfbook_set.count()
+    number_of_books.short_description = 'Number of Books'
+
+
 admin.site.register(ShelfBook)
