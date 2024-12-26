81f29f04ec6d3c2afa4c3fc64828165484f7a269
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-26 17:50:18 +0200

    Add serializers and update shelf URLs and views for improved data handling

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from rest_framework import serializers
-from .models import *
+from .models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -8,3 +8,21 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'email',
                   'first_name', 'last_name', 'date_joined', 'image']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'bio', 'image']
+        # Hide the password when sending the data
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            image=validated_data.get('image', 'default_user_image.png')
+        )
+        return user
