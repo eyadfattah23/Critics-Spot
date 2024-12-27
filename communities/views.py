5ee03478bfe90d47675cc3424df1466cb07d9c36
root <root@DESKTOP-DKDVO6G.>
2024-12-27 20:49:34 +0200

    creating a route for returning the posts of the community

--- communities/views.py
+++ communities/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Community
-from .serializers import CustomCommunitySerializer
+from .serializers import CustomCommunitySerializer, PostSerializer, Post
 
 
 @api_view()
 def communities_list(request):
     """ Return all the communities. """
     communities = Community.objects.prefetch_related('members', 'owner').all()
-    serializer = CustomCommunitySerializer(communities, many=True, context={'request': request})
+    serializer = CustomCommunitySerializer(
+        communities,
+        many=True,
+        context={'request': request}
+    )
     return Response(serializer.data)
 
 
@@ -17,5 +21,16 @@ def communities_list(request):
 def community_details(request, pk):
     """Return the details of a specific community. """
     community = get_object_or_404(Community, pk=pk)
-    serializer = CustomCommunitySerializer(community, context={'request': request})
+    serializer = CustomCommunitySerializer(
+        community,
+        context={'request': request})
+    return Response(serializer.data)
+
+
+@api_view()
+def community_posts(request, pk):
+    """Return the posts of a specific community. """
+    community = get_object_or_404(Community, pk=pk)
+    posts = Post.objects.filter(community=community)
+    serializer = PostSerializer(posts, many=True, context={'request': request})
     return Response(serializer.data)
