97c96e5db54849c088131f6502411f5820c43f48
root <root@DESKTOP-DKDVO6G.>
2024-12-27 21:24:17 +0200

    adding a route that gets the community posts comments

--- communities/urls.py
+++ communities/urls.py
@@ -3,6 +3,18 @@ from . import views
 
 urlpatterns = [
     path('communities/', views.communities_list, name='communities-list'),
-    path('communities/<int:pk>', views.community_details, name='community-details'),
-    path('communities/<int:pk>/posts', views.community_posts, name='community-posts'),
+    path(
+        'communities/<int:pk>',
+        views.community_details,
+        name='community-details'
+    ),
+    path(
+        'communities/<int:pk>/posts', views.community_posts,
+        name='community-posts'
+    ),
+    path(
+        'communities/<community_id>/posts/<post_id>/comments',
+        views.community_posts_comments,
+        name='post-comments'
+    ),
 ]
