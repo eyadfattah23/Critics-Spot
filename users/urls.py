34474120479d3af28e51fe877fccce2c3317bf7f
root <root@DESKTOP-DKDVO6G.>
2024-12-28 13:36:02 +0200

    adding a name in the url path of the users/id in the users.urls file

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('users/', views.users_list),
-    path('users/<int:pk>', views.user_details),
+    path('users/<int:pk>', views.user_details, name='user-details'),
 ]
