acdffb3344be6f4e9a6d9784f4bddbf70ac7b49c
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-29 12:33:25 +0200

    adding a urls.py file to the communities app and creating simple 2 views methods to test the connection

--- communities/views.py
+++ communities/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view()
+def communities_list(request):
+    """ Return all the shelves. """
+    return Response("Hello, World!")
+
+
+@api_view()
+def community_details(request, id):
+    """ Return the users shelves from the id of the user. """
+    return Response(id)
