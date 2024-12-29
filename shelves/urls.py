cd306deabc48ad59fb11ba0a467b47134a5b6a7e
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-29 12:35:39 +0200

    Add create shelf functionality with serializer and URL routing

--- shelves/urls.py
+++ shelves/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import shelves_list, shelf_details
+from .views import shelves_list, shelf_details, create_shelf
 import users.views
 
 urlpatterns = [
     path('shelves/', shelves_list, name='shelves-list'),
     path('shelves/<int:pk>/', shelf_details, name='shelf-details'),
     path('users/<int:pk>/', users.views.user_details, name='user-details'),
+    path('shelves/create/<int:user_id>/', create_shelf, name='create-shelf'),
 ]
