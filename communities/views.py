dc5a059252367dd9f479a0c020d57510f3ce4431
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-30 16:57:17 +0200

    sorting data

--- communities/views.py
+++ communities/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-
 from .models import Community, Post, Comment, Like
 from .serializers import *
 from users.models import CustomUser
@@ -14,8 +13,9 @@ class CommunityViewSet(ModelViewSet):
     """ Handels all communities' routes. """
     queryset = Community.objects.all().select_related('owner').prefetch_related('members', 'posts')
     serializer_class = CustomCommunitySerializer
-    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filter_backends = [SearchFilter, DjangoFilterBackend, OrderingFilter]
     search_fields = ['name', 'description']
+    ordering_fields = ['name', 'created_at']
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -25,6 +25,8 @@ class CommunityPostViewSet(ModelViewSet):
     """ Handels all the routes posts of a specific community. """
     queryset = Post.objects.all().select_related('user', 'community').prefetch_related('likes', 'comments')
     serializer_class = PostSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['updated_at']
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -34,7 +36,8 @@ class CommunityPostCommentsViewSet(ModelViewSet):
     """ Handels all the routes comments of a specific post in a specific community. """
     queryset = Comment.objects.all().select_related('user', 'post')
     serializer_class = CommunityPostCommentSerializer
-
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['updated_at']
     def get_serializer_context(self):
         return {'request': self.request}
 
@@ -43,6 +46,8 @@ class CommunityPostLikesViewSet(ModelViewSet):
     """ Handels all the routes likes of a specific post in a specific community. """
     queryset = Like.objects.all().select_related('user', 'post')
     serializer_class = LikeSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_at']
 
     def get_serializer_context(self):
         return {'request': self.request}
